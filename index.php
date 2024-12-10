6629c7bb1fc160fe3b8a72c5a257a8ae1bc1f414
Meybili <meybili200219@gmail.com>
2024-12-09 23:33:17 -0500

    feat: :fire: Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-Type: application/json');
 
-// Function for RPC
+// HelloWorld function for RPC
 function helloWorld($name) {
-    return "Hello $name!";
+    return "Hello, $name!";
 }
 
-// Process the RPC call
+//  Process the RPC call
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = json_decode(file_get_contents('php://input'), true);
     if (isset($input['method']) && $input['method'] === 'helloWorld' && isset($input['params'][0])) {
@@ -16,4 +16,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode(['jsonrpc' => '2.0', 'error' => 'Method not found', 'id' => 1]);
     }
 }
-
